c4421740c0293d6d258c2eda1e046801b71d8e43
ll6 <ll6@illinois.edu>
2019-10-18 17:49:47 -0500

    Fix 3.1

--- ece408_src/new-forward._copy.cuh
+++ ece408_src/new-forward._copy.cuh
@@ -1,3 +1,4 @@
+
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 
@@ -7,7 +8,9 @@ namespace mxnet
 {
 namespace op
 {
-const int TILE_WIDTH = 4;
+
+const TILE_WIDTH = 4;
+
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
@@ -17,8 +20,8 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     The goal here is to be correct AND fast.
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
-    
-    
+   /*
+
     int b1 = blockIdx.x;
     int b2 = blockIdx.y;
     int b3 = blockIdx.z;
@@ -34,35 +37,34 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     //(void)H_out; // silence declared but never referenced warning. remove this line when you start working
     //(void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
-    // An example use of these macros:
-    // float a = y4d(0,0,0,0)
-    // y4d(0,0,0,0) = a
-    
-    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+// An example use of these macros:
+// float a = y4d(0,0,0,0)
+// y4d(0,0,0,0) = a
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    if(h < H_out){
-        for (int w = 0; w < W_out; w++){
-            y4d(b,m,h,w) = 0;
-            for (int c = 0; c < C; c++)
+if(h < H_out){
+    for (int w = 0; w < W_out; w++){
+        y[b][m][h][w] = 0;
+        for (int c = 0; c < C; c++)
+        {
+            for (int p = 0; p < K; p++)
             {
-                for (int p = 0; p < K; p++)
+                for (int q = 0; q < K; q++)
                 {
-                    for (int q = 0; q < K; q++)
-                    {
-                        y4d(b,m,h,w) += x4d(b,c,(h+p),(w+q)) * k4d(m,c,p,q);
-                    }
+                    y4d(b,m,h,w) += x4d(b,c,(h+p),(w+q)) * k4d(m,c,p,q);
                 }
             }
         }
     }
+}
     
 
-    #undef y4d
-    #undef x4d
-    #undef k4d
-    
+#undef y4d
+#undef x4d
+#undef k4d
+*/
 
 }
 
@@ -77,26 +79,29 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    // CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+    //CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
+    /*
     const int B = x.shape_[0];
     const int M = y.shape_[1];
     const int C = x.shape_[1];
     const int H = x.shape_[2];
     const int W = x.shape_[3];
-    const int K = w.shape_[3];
+    const int K = k.shape_[3];
 
     // Set the kernel dimensions
-    dim3 gridDim(ceil(float(B)/float(TILE_WIDTH)),
-                 ceil(float(M)/float(TILE_WIDTH)),
-                 ceil(float(H)/float(TILE_WIDTH)));
+    dim3 gridDim(ceil((1.0*B)/(TILE_WIDTH*1.0)),
+                 ceil((1.0*M)/(TILE_WIDTH*1.0)),
+                 ceil((1.0*H)/(TILE_WIDTH*1.0)),
+                );
     dim3 blockDim(TILE_WIDTH,TILE_WIDTH,TILE_WIDTH);
 
     // Call the kernel
     forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
+    */
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
 }
