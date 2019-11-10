07e9307a154c0b70fba0a75b7192312fab0a3520
Mirari <zf4@illinois.edu>
2019-11-09 18:09:49 -0600

    try constant

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -42,7 +42,6 @@ __global__ void forward_kernel(float *y, const float *x, const int B, const int 
     
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
     #define K4d(i3, i2, i1, i0) MASK[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     __shared__ float subTile[TILE_WIDTH][TILE_WIDTH][TILE_WIDTH];
