825c953fdb74671491f10793a586e7400f6f311b
Mirari <zf4@illinois.edu>
2019-12-14 16:07:36 -0600

    remove restrict

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -9,7 +9,7 @@ namespace op
 {
 const int TILE_WIDTH = 16;
 
-__global__ void forward_kernel(float *__restrict__ y, const float *__restrict__ x, const float *__restrict__ k, const int B, const int M, const int C, const int H, const int W, const int K)
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
     /*
