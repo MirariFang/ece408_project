7aaeb4b48a61d9d98a7cd32dce395240acb71fdb
ll6 <ll6@illinois.edu>
2019-12-16 21:02:40 -0600

    Framework code

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -7,17 +7,59 @@ namespace mxnet
 {
 namespace op
 {
-const int TILE_WIDTH = 16;
+const int TILE_WIDTH_N = 4;
+const int TILE_WIDTH_M = 8;
+const int STEPS = 2;
+const int NUM_REG = TILE_WIDTH_M/TILE_WIDTH_N;
 
+/*
+Size of the unroll matrix would be C*K*K*H_out*W_out
+Size of the x matrix is M*(C*K*K)
+*/
+
+__global__ void forward_kernel(float *__restrict__ y, const float *__restrict__ x, const float *__restrict__ k, 
+                                const int B, const int M, const int C, const int H, const int W, const int K){
+
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+int bx = blockIdx.x;
+int by = blockIdx.y;
+int bz = blockIdx.z;
+int tx = threadIdx.x;
+int ty = threadIdx.y;
+int tz = threadIdx.z;
+int gridSize = gridDim;
+__shared__ float tileMatXUnroll[STEPS][TILE_WIDTH_N];
+int Reg[STEPS];
+int numIter = ceil((C*K*K)/STEPS);
+int w_row_start;
+int w_col_start;
+int x_row_start;
+int x_col_start;
+int y_row_start;
+int y_col_start;
+for(int i=0;i<numIter;i++){
+    /*Load*/
+    for(int j=0;j<STEPS;j++)
+        Reg[j] = w_row_num+i*(bl);
+        tileMatXUnroll[j] = x4d(_, _, x_col_start, x_rol_start);
+        
+    for(int j=0;j<TILE_WIDTH_N;j++){
+        for(int k)
+            y4d(Y_b, Y_m, Y_h, Y_w) += Reg[]*tileMatXUnroll[k][];
+    }
+}
+
+#undef y4d
+#undef x4d
+#undef k4d
+}
+
+/*
+const int TILE_WIDTH = 16;
 __global__ void forward_kernel(float *__restrict__ y, const float *__restrict__ x, const float *__restrict__ k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
 
     int bx = blockIdx.x;
     int by = blockIdx.y;
@@ -99,6 +141,8 @@ __global__ void forward_kernel(float *__restrict__ y, const float *__restrict__ 
 #undef k4d
 }
 
+*/
+
 /* 
    This function is called by new-inl.h
    Any code you write should be executed by this function.
@@ -119,23 +163,15 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int H = x.shape_[2];
     const int W = x.shape_[3];
     const int K = w.shape_[3];
-    /*
-    printf("B: %d\n", B);
-    printf("M: %d\n", M);
-    printf("C: %d\n", C);
-    printf("H: %d\n", H);
-    printf("W: %d\n", W);
-    printf("K: %d\n", K);
-    */
     // Set the kernel dimensions
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
     dim3 gridDim(ceil(H_out * W_out / (1.0 * TILE_WIDTH)),
-                 ceil(M / (1.0 * TILE_WIDTH)),
+                 M,
                  B);
-    dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
+    dim3 blockDim(TILE_WIDTH_M,1,1);
 
-    // Call the kernel
+    // Call the kernel Assume x is unrolled
     forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_,B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
